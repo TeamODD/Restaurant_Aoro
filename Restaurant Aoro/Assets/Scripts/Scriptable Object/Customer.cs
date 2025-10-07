85b46397b3b3db454d2bfd6718ad0c4ac7f6606f
UTSU2 <ejy6125@gmail.com>
2025-10-07 21:11:34 +0900

    fix : Customer 데이터 수정 및 추가
    손님 필요 데이터들 모두 추가

--- Restaurant Aoro/Assets/Scripts/Scriptable Object/Customer.cs
+++ Restaurant Aoro/Assets/Scripts/Scriptable Object/Customer.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
 using System.Collections.Generic;
 public enum ResultType { Success, Fail, Perfect, Excellent, Late, WrongOrder}
+public enum TribeType { Human, Youkai }
+public enum NPCType { Common, Special }
+
+public enum FoodTaste
+{
+    Sweet,   
+    Salty,   
+    Spicy,     
+    Sour,     
+    Bitter,   
+}
+
+public enum FoodType
+{
+    Noodle,   
+    Soup,   
+    Meat,     
+    Dessert,  
+    Seafood,  
+    Vegetable, 
+    Drink    
+}
 
 [System.Serializable]
 public class ResultBucket
@@ -12,8 +34,19 @@ public class ResultBucket
 [CreateAssetMenu(menuName ="Customer")]
 public class Customer : ScriptableObject
 {
+    [Header("손님 정보")]
     public string CustomerID;
     public string CustomerName;
+    public TribeType tribe;
+    public NPCType NPCType;
+    [Range(0f, 24f)] public float appearStartHour = 17f;
+    [Range(0f, 24f)] public float appearEndHour = 20f;
+    [TextArea(3, 8)] public string codexDescription;
+    public List<FoodTaste> favoriteTastes = new();
+    public List<FoodTaste> dislikedTastes = new();
+    public List<FoodType> favoriteFoods = new();
+    public List<FoodType> dislikedFoods = new();
+    public int payable;
 
     [Header("Animations")]
     public AnimationClip frontAnim;
@@ -23,14 +56,9 @@ public class Customer : ScriptableObject
     public AnimationClip eatingAnim;
     public AnimationClip upAnim;
 
-    /*public Sprite leftSprite;
-    public Sprite frontSprite;
-    public Sprite rightSprite;
-    public Sprite SeatedSprite;
-    public Sprite EatingSprite;*/
-
-    [Header("대사")]
+    [Header("입장 대사")]
     [TextArea(2, 5)] public List<string> greetingLines = new();
+    [Header("주문 대사")]
     [TextArea(2, 5)] public List<string> orderLines = new();
 
     [Header("결과 대사 (타입별)")]
