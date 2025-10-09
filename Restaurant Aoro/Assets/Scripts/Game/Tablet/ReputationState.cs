8c6f48b4f73dcf89e5942c8f827b02e4ac68bec2
UTSU2 <ejy6125@gmail.com>
2025-10-10 00:52:14 +0900

    feat : 평판 기능 추가
    스프라이트 설정 후 평판에 따른 아이콘 교체 기능 추가

--- Restaurant Aoro/Assets/Scripts/Game/Tablet/ReputationState.cs
+++ Restaurant Aoro/Assets/Scripts/Game/Tablet/ReputationState.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 public class ReputationState : MonoBehaviour
 {
     [Header("평판 아이콘")]
-    public Sprite Human_1;
-    public Sprite Human_2;
-    public Sprite Human_3;
+    public Sprite Customer_1;
+    public Sprite Customer_2;
+    public Sprite Customer_3;
     public Sprite Youkai_1;
     public Sprite Youkai_2;
     public Sprite Youkai_3;
 
-    private double Youkai_Human_reputation;
-    private double Customer_reputation;
+    private int Youkai_Human_reputation = 0;
+    private int Customer_reputation = -100;
+    private Sprite CustomerSprite;
+    private Sprite YoukaiSprite;
 
-    
+    public void setSprite()
+    {
+        if(Youkai_Human_reputation >= 33)
+        {
+            YoukaiSprite = Youkai_3;
+        }
+        else if (Youkai_Human_reputation >= -33)
+        {
+            YoukaiSprite = Youkai_2;
+        }
+        else
+        {
+            YoukaiSprite = Youkai_1;
+        }
+
+        if (Customer_reputation >= 33)
+        {
+            CustomerSprite = Customer_3;
+        }
+        else if (Customer_reputation >= -33)
+        {
+            CustomerSprite = Customer_2;
+        }
+        else
+        {
+            CustomerSprite = Customer_1;
+        }
+    }
+
+    public void addCustomerReputation(int value)
+    {
+        Customer_reputation += value;
+    }
+
+    public void addYoukaiReputation(int value)
+    {
+        Youkai_Human_reputation += value;
+    }
 }
