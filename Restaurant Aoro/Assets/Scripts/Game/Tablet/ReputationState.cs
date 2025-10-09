d4ae5b92a6f98245a9566b91cb4b2546ded4afa9
UTSU2 <ejy6125@gmail.com>
2025-10-09 23:54:20 +0900

    feat : 평판 시스템 추가
    기본만

---
+++ Restaurant Aoro/Assets/Scripts/Game/Tablet/ReputationState.cs
@@ -0,0 +1,18 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ReputationState : MonoBehaviour
+{
+    [Header("평판 아이콘")]
+    public Sprite Human_1;
+    public Sprite Human_2;
+    public Sprite Human_3;
+    public Sprite Youkai_1;
+    public Sprite Youkai_2;
+    public Sprite Youkai_3;
+
+    private double Youkai_Human_reputation;
+    private double Customer_reputation;
+
+    
+}
