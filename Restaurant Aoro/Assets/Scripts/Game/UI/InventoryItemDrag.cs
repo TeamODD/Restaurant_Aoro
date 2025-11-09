954b10979c54affa4ecee4fdc34480e3685d5d32
UTSU2 <ejy6125@gmail.com>
2025-11-09 20:46:31 +0900

    tmp

--- Restaurant Aoro/Assets/Scripts/Game/UI/InventoryItemDrag.cs
+++ Restaurant Aoro/Assets/Scripts/Game/UI/InventoryItemDrag.cs
@@ -4,21 +4,44 @@ using UnityEngine.EventSystems;
 
 public class InventoryItemDrag : DraggingController, IPointerDownHandler, IPointerUpHandler
 {
+    public enum DropTarget { Cook, Plate }
+
     [SerializeField] private GameObject proxyPrefab;
+    [SerializeField] private GameObject plateProxyPrefab;
+
+    [SerializeField] private bool autoSelectByItemType = true;
+    [SerializeField] private DropTarget forcedTarget = DropTarget.Cook;
     private GameObject proxyObj;
     private Item item;
 
     public void Init(Item _item)
     {
         item = _item;
+    
     }
-
+    public void UseAutoTargetByItemType(bool useAuto) => autoSelectByItemType = useAuto;
     public void OnPointerDown(PointerEventData eventData)
     {
         isDragging = true;
         if (proxyObj) return;
-        proxyObj = Instantiate(proxyPrefab);
-        proxyObj.GetComponent<IngredientHitCooktile>().Init(item, gameObject);
+        DropTarget target = forcedTarget;
+        if (autoSelectByItemType && item != null)
+            target = (item.ItemType == ItemType.Ingredient) ? DropTarget.Cook : DropTarget.Plate;
+
+        GameObject prefab = (target == DropTarget.Cook) ? proxyPrefab : plateProxyPrefab;
+        if (prefab == null)
+        {
+            isDragging = false;
+            return;
+        }
+
+        proxyObj = Instantiate(prefab);
+
+        var cook = proxyObj.GetComponent<IngredientHitCooktile>();
+        if (cook != null) cook.Init(item, gameObject);
+
+        var plate = proxyObj.GetComponent<FoodHitPlateTile>();
+        if (plate != null) plate.Init(item, gameObject);
     }
 
     public void OnPointerUp(PointerEventData eventData)
